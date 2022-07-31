df2bdf4e6819e1f4283093f5475273d318a9bbe4
Hg <dev@indigo.re>
2022-07-31 22:54:43 +0200

    lizardclock-currenttime.sh: fix most shellcheck warnings
    next are harder to fix since we want some empty args not to count as an arg
    at all

--- lizardclock-currenttime.sh
+++ lizardclock-currenttime.sh
@@ -20,20 +20,20 @@ moonphase () {
 	MOT=2953058
 	MOS=486700
 	intfactor=100000
-	jd=`expr $jd \* $intfactor`
-	age=`expr \( $jd + $MOS \) \* $intfactor / $MOT`
-	age=`expr 2 \* \( $age - \( \( $age / $intfactor \) \* $intfactor \) \) - $intfactor`
+	jd=$((jd * intfactor))
+	age=$(( ( jd + MOS ) * intfactor / MOT))
+	age=$(( 2 * ( age - ( ( age / intfactor ) * intfactor ) ) - intfactor))
 	if [ $age -lt 0 ]
 	then
-		age=`expr $MOT \* \( $intfactor + $age / 2 \) / $intfactor`
+		age=$(( MOT * ( intfactor + age / 2 ) / intfactor))
 	else
-		age=`expr \( $age \* $MOT / 2 \) / $intfactor`
+		age=$(( ( age * MOT / 2 ) / intfactor))
 	fi
 	if [ $age -ge $MOT ]
 	then
 		age=0
 	fi
-	expr $age / $intfactor + 1
+	echo $(( age / intfactor + 1 ))
 }
 
 zipf=$1
@@ -41,33 +41,37 @@ shift
 
 output=$PWD/output.png
 
-set -- `LANG=C date +"%m %d %u %P %H %I %M %j"`
+set -- $(LANG=C date +"%m %d %u %P %H %I %M %j")
 month=month${1#0}.png; shift
 day=day${1#0}.png; shift
 weekday=weekday$1.png; shift
 ampm=$1.png; shift
 hours24=hour${1#0}.png; shift
-hours12=hour${1#0}.png; hourshand=`expr $1 \* 60`; shift
-minutes=minute${1#0}.png; hourshand=`expr $1 + $hourshand`; shift
-moonphase=moonphase`moonphase $1`.png; shift
-hourshand=hour`expr $hourshand / 12`.png
+hours12=hour${1#0}.png; hourshand=$(( $1 * 60 )); shift
+minutes=minute${1#0}.png; hourshand=$(( $1 + hourshand )); shift
+moonphase=moonphase$(moonphase "$1").png; shift
+hourshand=hour$((hourshand / 12)).png
 
 if [ -d "$zipf" ]
 then
 	dir=$zipf
 else
-	dir=`mktemp -d "clock.XXXXXX"`
+	dir=$(mktemp -d "clock.XXXXXX")
 	[ -z "$dir" ] && exit 1
 
 	# exit 11 means some files not in the zipfile
-	unzip -qL "$zipf" -d "$dir" $month $day $weekday $ampm $hours24 ${hours12#$hours24} $minutes $hourshand $moonphase clock.ini bg.jpg || [ $? -eq 11 ]
+	unzip -qL "$zipf" -d "$dir" \
+		"$month" "$day" "$weekday" "$ampm" \
+		"$hours24" ${hours12#$hours24} "$minutes" "$hourshand" "$moonphase" \
+		clock.ini bg.jpg \
+		|| [ $? -eq 11 ]
 	#tar -x -f"$zipf" -C "$dir" $month $day $weekday $ampm $hours24 ${hours12#$hours24} $minutes clock.ini bg.jpg
 fi
 cd "$dir"
 
 if grep -q 'ampmenabled[[:space:]]*=[[:space:]]*1' clock.ini
 then
-	haspm=`date +%p`
+	haspm=$(date +%p)
 	hours=${haspm:+$hours12}
 	hours=${hours:-$hours24}
 else
@@ -82,9 +86,9 @@ fi
 set --
 if [ -f "$moonphase" ]
 then
-	set -- $moonphase -composite
+	set -- "$moonphase" -composite
 fi
-convert bg.jpg "$@" $month -composite $weekday -composite $day -composite $hours  -composite $minutes -composite ${haspm:+$ampm} ${haspm:+-composite} $output
+convert bg.jpg "$@" "$month" -composite "$weekday" -composite "$day" -composite "$hours" -composite "$minutes" -composite ${haspm:+$ampm} ${haspm:+-composite} "$output"
 
 cd -
 rm -rf "$dir"
